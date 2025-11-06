6e38d5c778e9ffc1244acb5e3d2843584f0a1263
TFISH <fishertyler1991@gmail.com>
2025-11-05 19:51:30 -0700

    Project 3 and template

---
+++ templates/templateProject.py
@@ -0,0 +1,11 @@
+from enum import Enum
+from random import random
+
+
+subProjectNames = ["sp1", "sp2", "sp3"]
+inputString = "Enter sub project: \n"
+subProjectNum = len(subProjectNames)
+for i in range(subProjectNum):
+    inputString += str(i) + ": " + subProjectNames[i] + "\n"
+inputString += "??: "
+subProjectRunMode = input(inputString)
