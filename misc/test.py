6acc8705dc27c056bbb66317e4a7a155c1e166f4
TFISH <fishertyler1991@gmail.com>
2025-11-28 20:54:19 -0700

    Finished p6 and start p7.

--- misc/test.py
+++ misc/test.py
@@ -1,4 +1,80 @@
-eggs = "test1"
-bacon = "TEST"
+import random
+import time
+import shutil
 
-assert eggs.lower() is bacon.lower() 
+def _hackerMode() -> None:
+    """
+    THE ONE. True Matrix rain with everything:
+    - Full katakana + binary
+    - Bright white heads
+    - Block characters (█ ▓ ░ ▒)
+    - Dense, no gaps
+    - Works in any terminal
+    """
+    # All the characters from the actual Matrix
+    chars = "01ｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜｦﾝ"
+    blocks = "█▓▒░▉▊▋▌▍▎"
+
+    try:
+        w, h = shutil.get_terminal_size((80, 24))
+        w = max(w, 50)
+        h = max(h, 15)
+
+        # One drop per column
+        pos = [random.randint(-h*2, -1) for _ in range(w)]
+        trail_len = [random.randint(12, 30) for _ in range(w)]
+
+        while True:
+            screen = [[" "] * w for _ in range(h)]
+
+            for x in range(w):
+                pos[x] += 1
+
+                # Respawn when off-screen
+                if pos[x] > h + 5:
+                    pos[x] = random.randint(-20, -3)
+                    trail_len[x] = random.randint(14, 32)
+
+                y = pos[x]
+                if 0 <= y < h:
+                    # Bright white head (20% chance)
+                    if random.random() < 0.2:
+                        screen[y][x] = "█"
+                    else:
+                        screen[y][x] = random.choice(chars + blocks)
+
+                    # Epic trail with blocks + katakana + fading
+                    for i in range(1, trail_len[x]):
+                        ty = y - i
+                        if ty < 0 or ty >= h:
+                            break
+
+                        roll = random.random()
+                        if i == 1:
+                            screen[ty][x] = random.choice(chars + "▓▒")
+                        elif i < 4:
+                            screen[ty][x] = random.choice("░▒▊▋" + chars)
+                        elif i < 8:
+                            if roll < 0.6:
+                                screen[ty][x] = random.choice("░▒·.")
+                            else:
+                                screen[ty][x] = random.choice(chars + blocks)
+                        else:
+                            if roll < 0.25:
+                                screen[ty][x] = random.choice("·.")
+                            elif roll < 0.4:
+                                screen[ty][x] = random.choice("░▒")
+
+            # Bright green + full redraw
+            print("\033[92m\033[H" + "\n".join("".join(row) for row in screen), end="", flush=True)
+            time.sleep(0.05)
+
+    except KeyboardInterrupt:
+        print("\033[0m\033[H\033[J", end="")
+        _clearConsole()
+        print("Wake up, Neo...")
+        time.sleep(1)
+        _clearConsole()
+
+# JUST RUN THIS ONE LINE:
+_hackerMode()
--- project6/project6.py
+++ project6/project6.py
@@ -1,5 +1,19 @@
 from enum import Enum
 import random
+import time
+
+def _getNumberFromText(i, numberType = int, printExcept = False):
+    toReturn = None
+    try:
+        if numberType == int:
+            toReturn = int(i)
+        elif numberType == float:
+            toReturn = float(i)
+    except ValueError:
+        if printExcept:
+            print("Value is not " + str(numberType) + ".")
+        pass
+    return toReturn
 
 ballResponses = ["It is certain.",
     "It is decidedly so.",
@@ -23,15 +37,70 @@ ballResponses = ["It is certain.",
     "Very doubtful."]
 
 def _SP1():
+    theQ = input("Ask the ball if you dare.\n??: ")
+    print(random.choice(ballResponses))
+    if "yes" in input("\nAsk again?\n??: ").lower():
+        _SP1()
     pass
 
 def _SP2():
+    matWidth = 70
+    try:
+        cols = [0] * matWidth
+        while True:
+            for _i in range(matWidth):
+                if random.random() < 0.02:
+                    cols[i] = random.randint(4, 14)
+                if cols[i] <= 0:
+                    print(" ", end = '')
+                else:
+                    print(random.choice([0, 1]), end = '')
+                    cols[i] -= 1
+            print()
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        pass
     pass
 
 def _SP3():
+    spam = ['apples', 'bananas', 'tofu', 'cats']
+    printVal = ""
+    for i in range(len(spam)):
+        if i == len(spam) - 2:
+            printVal += spam[i] + ", and "
+        elif i == len(spam) - 1:
+            printVal += spam[i] + "."
+        else:
+            printVal += spam[i] + ", "
+    print(printVal)
     pass
 
-subProjectNames = ["Listed balls", "sp2", "sp3"]
+def _SP4():
+    coinRes = []
+    tossOps = ['H', 'T']
+    for i in range(100):
+        coinRes.append(random.choice(tossOps))
+        pass
+    print(coinRes)
+    curStreakOp = coinRes[0]
+    curStreakCount = 1
+    maxStreakOp = coinRes[0]
+    maxStreakCount = 1
+    maxStreakIndex = 0
+    for i in range(1, len(coinRes)):
+        if coinRes[i] == curStreakOp:
+            curStreakCount += 1
+        else:
+            curStreakOp = coinRes[i]
+            if curStreakCount > maxStreakCount:
+                maxStreakCount = curStreakCount
+                maxStreakOp = curStreakOp
+                maxStreakIndex = i
+            curStreakCount = 1
+    print("Largest streak is of " + maxStreakOp + " at " + str(maxStreakCount) + " occuring at index " + str(maxStreakIndex) + ".")
+    pass
+
+subProjectNames = ["Listed balls", "I'm a Hacker", "BQ: comma code", "BQ: coin streaks"]
 inputString = "Enter sub project (0 or 'exit' to exit): \n"
 subProjectNum = len(subProjectNames)
 for i in range(subProjectNum):
@@ -61,3 +130,49 @@ while not endProgram:
         _SP2()
     elif subProjectRunMode == 3:
         _SP3()
+    elif subProjectRunMode == 4:
+        _SP4()
+
+'''
+  1.  What is []?
+    an empty list
+  2.  How would you assign the value 'hello' as the third value in a list stored in a variable named spam? (Assume spam contains [2, 4, 6, 8, 10].)
+    spam[2] = "hello"
+For the following three questions, assume spam contains the list ['a', 'b', 'c', 'd'].
+
+  3.  What does spam[int(int('3' * 2) // 11)] evaluate to?
+    'd'
+  4.  What does spam[-1] evaluate to?
+    'd'
+  5.  What does spam[:2] evaluate to?
+    ['a', 'b']
+For the following three questions, assume bacon contains the list [3.14, 'cat', 11, 'cat', True].
+
+  6.  What does bacon.index('cat') evaluate to?
+    1
+  7.  What does bacon.append(99) make the list value in bacon look like?
+    adds 99 to the end after True
+  8.  What does bacon.remove('cat') make the list value in bacon look like?
+    [3.14, 11, 'cat', True]
+  9.  What are the operators for list concatenation and list replication?
+    extend and copy
+10.  What is the difference between the append() and insert() list methods?
+    insert takes an extra argument specfiying were to insert the new data
+11.  What are two ways to remove values from a list?
+    remove or del[index]
+12.  Name a few ways that list values are similar to string values.
+    both point to an ordered collection of data instead of a single value
+13.  What is the difference between lists and tuples?
+    tuples are immutable (can't be changed)
+14.  How do you write the tuple value that has just the integer value 42 in it?
+    (42,)
+15.  How can you get the tuple form of a list value? How can you get the list form of a tuple value?
+    use list() or tuple()
+16.  Variables that “contain” list values don't actually contain lists directly. What do they contain instead?
+    pointers to the data
+17.  What is the difference between copy.copy() and copy.deepcopy()?
+    deepcopy copies list in list values
+
+
+
+'''
